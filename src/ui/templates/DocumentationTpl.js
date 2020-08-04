c49ff3d71cdff99d3dd1d90f16d9ea17dc27742e
Piotr F <hello@piotrf.pl>
2020-08-04 20:55:06 +0200

    Fix some warnings.

--- src/ui/templates/DocumentationTpl.js
+++ src/ui/templates/DocumentationTpl.js
@@ -8,7 +8,7 @@ import List from '@material-ui/core/List';
 import ListItem from '@material-ui/core/ListItem';
 import ListItemText from '@material-ui/core/ListItemText';
 import SwipeableDrawer from '@material-ui/core/Drawer';
-import Toolbar from '@material-ui/core/Toolbar';
+// import Toolbar from '@material-ui/core/Toolbar';
 import { makeStyles } from '@material-ui/core/styles';
 
 const drawerWidth = 240;
@@ -62,7 +62,7 @@ export default function DocumentationTpl({ children, ...props }) {
                 className={classes.drawer}
                 classes={{ paper: classes.drawerPaper }}
                 variant="permanent">
-                <div class={classes.toolbar} />
+                <div className={classes.toolbar} />
                 <List>
                   {allDocumentationPages.map(({ node }) => {
                     return (
