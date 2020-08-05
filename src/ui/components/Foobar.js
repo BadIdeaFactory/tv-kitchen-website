45dc2c14b9252e511a73e61c10bd51ee2945954b
Piotr F <hello@piotrf.pl>
2020-08-05 18:54:10 +0200

    Clean up.

--- src/ui/components/Foobar.js
+++ src/ui/components/Foobar.js
@@ -1,11 +1,6 @@
 import React from 'react';
 
-// import { makeStyles } from '@material-ui/core/styles';
-
-// const useStyles = makeStyles(theme => ({}));
-
 export default function Foobar(props) {
-  // const classes = useStyles();
   return (
     <>
       <h1>Foobar</h1>
