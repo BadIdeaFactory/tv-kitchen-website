64c2b3c3b5b510d8cd9d565a8d74c8782c86d116
Piotr F <hello@piotrf.pl>
2020-08-05 18:10:52 +0200

    Add basic Navbar.

--- src/ui/components/Layout.js
+++ src/ui/components/Layout.js
@@ -1,20 +1,29 @@
 import React from 'react';
 
 import CssBaseline from '@material-ui/core/CssBaseline';
-import { ThemeProvider } from '@material-ui/core/styles';
+import { makeStyles, ThemeProvider } from '@material-ui/core/styles';
 
 import DefaultThm from '@ui/themes/DefaultThm';
 import Foobar from '@ui/components/Foobar';
+import Navbar from '@ui/components/Navbar';
 import Topbar from '@ui/components/Topbar';
 
+const useStyles = makeStyles(theme => ({
+  toolbar: theme.mixins.toolbar,
+}));
+
 export default function Layout(props) {
+  const classes = useStyles();
   const { children } = props;
   return (
     <ThemeProvider theme={DefaultThm}>
       <CssBaseline />
       <Topbar />
+      <div className={classes.toolbar} />
       {children}
+      <div className={classes.toolbar} />
       <Foobar />
+      <Navbar />
     </ThemeProvider>
   );
 }
---
+++ src/ui/components/Navbar.js
@@ -0,0 +1,59 @@
+import PropTypes from 'prop-types';
+import React, { useState } from 'react';
+import { Link as GatsbyLink } from 'gatsby';
+
+import BottomNavigation from '@material-ui/core/BottomNavigation';
+import BottomNavigationAction from '@material-ui/core/BottomNavigationAction';
+import FavoriteBorderIcon from '@material-ui/icons/FavoriteBorder';
+import HelpOutlineIcon from '@material-ui/icons/HelpOutline';
+import HomeOutlinedIcon from '@material-ui/icons/HomeOutlined';
+import InfoOutlinedIcon from '@material-ui/icons/InfoOutlined';
+import MenuBookOutlinedIcon from '@material-ui/icons/MenuBookOutlined';
+import Slide from '@material-ui/core/Slide';
+import useScrollTrigger from '@material-ui/core/useScrollTrigger';
+import { makeStyles } from '@material-ui/core/styles';
+
+const useStyles = makeStyles(theme => ({
+  root: {
+    left: 0,
+    position: 'fixed',
+    right: 0,
+    top: 'auto',
+    bottom: 0,
+    [theme.breakpoints.up('md')]: {
+      display: 'none',
+    },
+  },
+  toolbar: theme.mixins.toolbar,
+}));
+
+function HideOnScroll(props) {
+  const { children } = props;
+  const trigger = useScrollTrigger();
+
+  return (
+    <Slide appear={false} direction="up" in={!trigger}>
+      {children}
+    </Slide>
+  );
+}
+
+HideOnScroll.propTypes = {
+  children: PropTypes.element.isRequired,
+};
+
+export default function Navbar(props) {
+  const classes = useStyles();
+  return (
+    <>
+      <div className={classes.toolbar} />
+      <BottomNavigation className={classes.root} showLabels>
+        <BottomNavigationAction component={GatsbyLink} to="/" label="Home" icon={<HomeOutlinedIcon />} />
+        <BottomNavigationAction component={GatsbyLink} to="/about" label="About" icon={<InfoOutlinedIcon />} />
+        <BottomNavigationAction component={GatsbyLink} to="/partners" label="Partners" icon={<FavoriteBorderIcon />} />
+        <BottomNavigationAction component={GatsbyLink} to="/docs" label="Docs" icon={<MenuBookOutlinedIcon />} />
+        <BottomNavigationAction component={GatsbyLink} to="/help" label="Help" icon={<HelpOutlineIcon />} />
+      </BottomNavigation>
+    </>
+  );
+}
