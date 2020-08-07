5397c724252b276f4e96a3064c5a8593e5317125
Piotr F <hello@piotrf.pl>
2020-08-07 21:32:19 +0200

    Reset fresh content setup w/ mdx.

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,7 +5,7 @@ import Layout from '@ui/components/Layout';
 export default function HomeRoute(props) {
   console.log({ props });
   return (
-    <Layout>
+    <Layout {...props}>
       <h1>Home</h1>
     </Layout>
   );
