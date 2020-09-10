163df702f0f42d9b27473601c082c5365f900d84
Piotr F <hello@piotrf.pl>
2020-09-11 00:29:08 +0200

    Flex-align Contact.js buttons.

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -36,6 +36,14 @@ const useStyles = makeStyles(theme => ({
   button: {
     marginTop: theme.spacing(3),
   },
+  gridItem: {
+    [theme.breakpoints.up('md')]: {
+      alignItems: 'flex-start',
+      display: 'flex',
+      flexDirection: 'column',
+      justifyContent: 'space-between',
+    },
+  },
 }));
 
 export default function Contact(props) {
@@ -44,36 +52,42 @@ export default function Contact(props) {
     <ThemeProvider theme={darkTheme}>
       <Paper className={`${classes.paper} dark-theme`}>
         <Grid container alignContent="stretch" spacing={8}>
-          <Grid item xs={12} md={4}>
-            <Typography variant="h4" component="h2" gutterBottom>
-              Join newsletter
-            </Typography>
-            <Typography variant="body1" gutterBottom>
-              Occasionally we send out emails to our faithful followers. Sign up to our newsletter bla bla.
-            </Typography>
+          <Grid className={classes.gridItem} item xs={12} md={4}>
+            <div>
+              <Typography variant="h4" component="h2" gutterBottom>
+                Join newsletter
+              </Typography>
+              <Typography variant="body1" gutterBottom>
+                Occasionally we send out emails to our faithful followers. Sign up to our newsletter bla bla.
+              </Typography>
+            </div>
             <Button className={classes.button} href={config.elsewhere.tinyletter.url} variant="outlined">
               Tune in
             </Button>
           </Grid>
-          <Grid item xs={12} md={4}>
-            <Typography variant="h4" component="h2" gutterBottom>
-              Catch on Slack
-            </Typography>
-            <Typography variant="body1" gutterBottom>
-              Join us chatting about this, this and that across a variety of entertaining and useful channels.
-            </Typography>
+          <Grid className={classes.gridItem} item xs={12} md={4}>
+            <div>
+              <Typography variant="h4" component="h2" gutterBottom>
+                Catch on Slack
+              </Typography>
+              <Typography variant="body1" gutterBottom>
+                Join us chatting about this, this and that across a variety of entertaining and useful channels.
+              </Typography>
+            </div>
             <Button className={classes.button} href={config.elsewhere.slack.url} variant="outlined">
               Join Slack
             </Button>
           </Grid>
-          <Grid item xs={12} md={4}>
-            <Typography component="h2" gutterBottom variant="h4">
-              Email us
-            </Typography>
-            <Typography variant="body1" gutterBottom>
-              For business enquiries or casual chatter—do drop us a line at{' '}
-              <Link href="mailto:tvkitchen@biffud.com">tvkitchen@biffud.com</Link>. We ♥︎ emails.
-            </Typography>
+          <Grid className={classes.gridItem} item xs={12} md={4}>
+            <div>
+              <Typography component="h2" gutterBottom variant="h4">
+                Email us
+              </Typography>
+              <Typography variant="body1" gutterBottom>
+                For business enquiries or casual chatter—do drop us a line at{' '}
+                <Link href="mailto:tvkitchen@biffud.com">tvkitchen@biffud.com</Link>. We ♥︎ emails.
+              </Typography>
+            </div>
             <Button className={classes.button} href="mailto:tvkitchen@biffud.com" variant="outlined">
               Email us
             </Button>
